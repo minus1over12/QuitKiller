f6539ed7606631e27f0fad2ce8ec5db3dc454e48
War Pigeon <54217755+minus1over12@users.noreply.github.com>
2024-07-01 23:22:10 -0500

    Make plugin

---
+++ README.txt
@@ -0,0 +1,2 @@
+Minecraft internally tracks who should be credited with killing someone if they die. This plugin
+uses that to check if a player logged out while being attacked, and kills them if they did.
---
+++ src/main/java/io/github/minus1over12/quitkiller/QuitKiller.java
@@ -0,0 +1,32 @@
+package io.github.minus1over12.quitkiller;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * A plugin that kills a player if they have a killer when they quit.
+ *
+ * @author War Pigeon
+ */
+public final class QuitKiller extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        getServer().getPluginManager().registerEvents(new Listener() {
+            /**
+             * Kills a player if they have a killer when they quit.
+             *
+             * @param event The event that is triggered when a player quits.
+             */
+            @EventHandler
+            public static void killQuitter(PlayerQuitEvent event) {
+                Player player = event.getPlayer();
+                if (player.getKiller() != null) {
+                    player.setHealth(0);
+                }
+            }
+        }, this);
+    }
+}
