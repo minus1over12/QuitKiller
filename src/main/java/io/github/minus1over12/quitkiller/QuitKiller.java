ffa3078f383d8b7d898e861437a4db4614132b5b
War Pigeon <54217755+minus1over12@users.noreply.github.com>
2024-07-10 15:23:48 -0500

    Infer nullity annotations

--- src/main/java/io/github/minus1over12/quitkiller/QuitKiller.java
+++ src/main/java/io/github/minus1over12/quitkiller/QuitKiller.java
@@ -5,6 +5,7 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.NotNull;
 
 /**
  * A plugin that kills a player if they have a killer when they quit.
@@ -21,7 +22,7 @@ public final class QuitKiller extends JavaPlugin {
              * @param event The event that is triggered when a player quits.
              */
             @EventHandler
-            public static void killQuitter(PlayerQuitEvent event) {
+            public static void killQuitter(@NotNull PlayerQuitEvent event) {
                 Player player = event.getPlayer();
                 if (player.getKiller() != null) {
                     player.setHealth(0);
